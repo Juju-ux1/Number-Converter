5a080b5404e45a395c000cec4226647b66e8c36e
Juju-ux1 <jamaicacutaran@SMU-0202>
2025-09-16 19:36:06 +0800

    Add project files.

---
+++ Number Converter/Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.btnConvert = New System.Windows.Forms.Button()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(0, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 13)
+        Me.Label1.TabIndex = 0
+        '
+        'txtInput
+        '
+        Me.txtInput.Location = New System.Drawing.Point(271, 124)
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(167, 20)
+        Me.txtInput.TabIndex = 1
+        Me.txtInput.Text = "Enter a number word:"
+        '
+        'btnConvert
+        '
+        Me.btnConvert.Location = New System.Drawing.Point(315, 150)
+        Me.btnConvert.Name = "btnConvert"
+        Me.btnConvert.Size = New System.Drawing.Size(75, 23)
+        Me.btnConvert.TabIndex = 2
+        Me.btnConvert.Text = "convert"
+        Me.btnConvert.UseVisualStyleBackColor = True
+        '
+        'lblResult
+        '
+        Me.lblResult.AutoSize = True
+        Me.lblResult.Location = New System.Drawing.Point(333, 193)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(0, 13)
+        Me.lblResult.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.btnConvert)
+        Me.Controls.Add(Me.txtInput)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtInput As TextBox
+    Friend WithEvents btnConvert As Button
+    Friend WithEvents lblResult As Label
+End Class
---
+++ Number Converter/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
+        Dim input As String = txtInput.Text.Trim().ToLower()
+        Dim numbers = New Dictionary(Of String, Integer) From {
+            {"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3}, {"four", 4},
+            {"five", 5}, {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9}, {"ten", 10}
+        }
+        Try
+            Dim number As Integer = If(numbers.ContainsKey(input), numbers(input), Convert.ToInt32(input))
+            lblResult.Text = "Converted number is: " & number
+            lblResult.ForeColor = Color.Green
+        Catch ex As Exception
+            lblResult.Text = "Error: Invalid number or number word."
+            lblResult.ForeColor = Color.Red
+        End Try
+
+    End Sub
+End Class
